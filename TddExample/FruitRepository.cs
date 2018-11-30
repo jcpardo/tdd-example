4ca324e5f8c4a0a2f46fca788823528803d230bb
Jose Pardo <Jose.Pardo@healthsun.com>
2018-11-29 22:03:06 -0500

    Refactored GetFruitWithLessCalories adding the logic.

--- TddExample/FruitRepository.cs
+++ TddExample/FruitRepository.cs
@@ -1,28 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace TddExample
 {
     public class FruitRepository
     {
-        private readonly List<Fruit> FruitList;
-
-        public FruitRepository()
+        private static readonly List<Fruit> fruitList = new List<Fruit>
         {
-            FruitList = new List<Fruit>
-            {
-                new Fruit { Name = "Banana", Calories = 89, GramsPer = 100, GeoDistribution = "Tropical" },
-                new Fruit { Name = "Apple", Calories = 52, GramsPer = 100, GeoDistribution = "Temperate" },
-                new Fruit { Name = "Pineapple", Calories = 50, GramsPer = 100, GeoDistribution = "Tropical" },
-                new Fruit { Name = "Grape", Calories = 67, GramsPer = 100, GeoDistribution = "SubTropical" },
-                new Fruit { Name = "Peach", Calories = 39, GramsPer = 100, GeoDistribution = "Temperate" }
-            };
-        }
+            new Fruit { Name = "Banana", Calories = 89, GramsPer = 100, GeoDistribution = "Tropical" },
+            new Fruit { Name = "Apple", Calories = 52, GramsPer = 100, GeoDistribution = "Temperate" },
+            new Fruit { Name = "Pineapple", Calories = 50, GramsPer = 100, GeoDistribution = "Tropical" },
+            new Fruit { Name = "Grape", Calories = 67, GramsPer = 100, GeoDistribution = "SubTropical" },
+            new Fruit { Name = "Peach", Calories = 39, GramsPer = 100, GeoDistribution = "Temperate" }
+        };
+
 
         public static Fruit GetFruitWithLessCalories(int calories)
         {
-            return new Fruit {Name = "Peach", Calories = 39, GramsPer = 100, GeoDistribution = "Temperate"};
+            return fruitList.First(x => x.Calories < calories);
         }
     }
 }
