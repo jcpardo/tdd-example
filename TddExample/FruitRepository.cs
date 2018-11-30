330544e4a956df38624f0b01825b0256aef0f18e
Jose Pardo <Jose.Pardo@healthsun.com>
2018-11-29 21:16:54 -0500

    Added GetFruitWithLessCalories method with no implementation

---
+++ TddExample/FruitRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TddExample
+{
+    public class FruitRepository
+    {
+        public static Fruit GetFruitWithLessCalories(int calories)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
