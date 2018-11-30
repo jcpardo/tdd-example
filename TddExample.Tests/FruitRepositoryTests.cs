b7a082f1a35a71ebe2048f4f890e6273755a5b91
Jose Pardo <Jose.Pardo@healthsun.com>
2018-11-29 23:24:09 -0500

    Added two tests to get a list of fruits or null given a geo-distribution

--- TddExample.Tests/FruitRepositoryTests.cs
+++ TddExample.Tests/FruitRepositoryTests.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Newtonsoft.Json;
 using Xunit;
 
@@ -28,6 +29,30 @@ namespace TddExample.Tests
         {
             var result = FruitRepository.ExistFruit("Mango");
             Assert.False(result);
+        }
+
+        [Fact]
+        public void GetFruits_GivenGeoDistribution_ReturnsFruitList()
+        {
+            var result = GetFruitsByGeoDistribution("Tropical");
+
+            var expectedResult = new List<Fruit>
+            {
+                new Fruit {Name = "Banana", Calories = 89, GramsPer = 100, GeoDistribution = "Tropical"},
+                new Fruit {Name = "Pineapple", Calories = 50, GramsPer = 100, GeoDistribution = "Tropical"}
+            };
+
+            var resultSerializedObject = JsonConvert.SerializeObject(result);
+            var expectedResultSerializedObject = JsonConvert.SerializeObject(expectedResult);
+
+            Assert.Equal(expectedResultSerializedObject, resultSerializedObject);
+        }
+
+        [Fact]
+        public void GetFruits_GivenGeoDistribution_ReturnsNull()
+        {
+            var result = GetFruitsByGeoDistribution("Wrong GeoDistribution");
+            Assert.Null(result);
         }
     }
 }
